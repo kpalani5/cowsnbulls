0c536418898283a8128590b4df793a7cca721e4b
kpalani5 <kpalani5@ncsu.edu>
2016-06-05 20:01:21 -0400

    Index Page Modification

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
 	<body>
 		<center>
 		<h1>
-			<?php echo("Welcome to Cows and Bulls!!"); ?>
+			<?php echo("COWS AND BULLS"); ?>
 		</h1>
+		</br></br>
+		<form action = "home.php" method = "post">
+		<table>
+			<tr> 
+				<td> Username </td> 
+				<td> <input type = "text" name = "username"> </td>
+			</tr>
+			<tr>
+				<td> Password </td> 
+				<td> <input type = "password" name = "password"> </td>
+			</tr>
+		</table>
+			<input type = "submit" value = "Login">
+		</form>
+		</br></br>
+		<a href = "register.php"> New to the game? Click here to register. </a> 
 		</center>
 	</body>
 </html>
