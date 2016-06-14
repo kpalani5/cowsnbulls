73cc6d6aeb57f7ba6a030bea62b17acc1f2226ef
kpalani5 <kpalani5@ncsu.edu>
2016-06-13 21:59:09 -0400

    Password Confirm

--- register.php
+++ register.php
@@ -9,24 +9,45 @@
 		//Check if username is available - PHP Ajax JQuery
 		//Check email format - HTML5, availability - PHP Ajax JQuery
 		//Check if password is confirmed - PHP for server side and JS for client side
+		if($_POST['password'] !== $_POST['confirm'])
+		{
+			
+		}
 		//Check password strength - PHP and JQuery
-		$cost = 10;
-		$salt = strtr(base64_encode(mcrypt_create_iv(16,MCRYPT_DEV_URANDOM)),'+','.');
-		$salt = sprintf("$2a$%02d$",$cost).$salt;
-		$hash = crypt($password,$salt);
-		$sql = "INSERT INTO login VALUES ('$username','$hash','user');";
-		$sql2 = "INSERT INTO user VALUES ('$username','$email',now());";
-		mysqli_query($db,$sql2) or die (mysqli_error($db));
-		mysqli_query($db,$sql) or die (mysqli_error($db));
-		
+		else
+		{
+			$cost = 10;
+			$salt = strtr(base64_encode(mcrypt_create_iv(16,MCRYPT_DEV_URANDOM)),'+','.');
+			$salt = sprintf("$2a$%02d$",$cost).$salt;
+			$hash = crypt($password,$salt);
+			$sql = "INSERT INTO login VALUES ('$username','$hash','user');";
+			$sql2 = "INSERT INTO user VALUES ('$username','$email',now());";
+			mysqli_query($db,$sql2);
+			mysqli_query($db,$sql);		 
+		}
 	}
 ?>
 <html>
+	<script>
+		function validate()
+		{
+			if(document.getElementById("password").value != document.getElementById("confirm").value)
+			{
+				document.getElementById('error').innerHTML = "Passwords do not match."
+				document.getElementById('password').value='';
+				document.getElementById('confirm').value='';
+			}
+		}
+		
+	</script>
 	<body>
 		<center>
 		<center> <h1> Registration </h1> </center>
 		<form action = "register.php" method = "post">
 			<table>
+			    <tr>
+					<td> <div id = "error"> </div> </td>
+				</tr>
 				<tr>
 					<td> Username: </td>
 					<td> <input type = "text" name = "username"> </td>
@@ -37,14 +58,14 @@
 				</tr>
 				<tr>
 					<td> Password: </td>
-					<td> <input type = "password" name = "password"> </td>
+					<td> <input type = "password" name = "password" id = "password"> </td>
 				</tr>
 				<tr>
 					<td> Confirm Password: </td>
-					<td> <input type = "password" name = "confirm"> </td>
+					<td> <input type = "password" name = "confirm" id = "confirm"> </td>
 				</tr>
 			</table>
-			<input type = "submit" value = "Register">
+			<input type = "submit" value = "Register" onclick = "validate(); return false;">
 		</form>
 		</center>
 	</body>
