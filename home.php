8243ee2e7d63ffef37f4eeed5685e90b09d5f1d8
kpalani5 <kpalani5@ncsu.edu>
2016-06-20 20:56:17 -0400

    game selection creation

--- home.php
+++ home.php
@@ -5,6 +5,12 @@
 	<body>
 		<center> <h2> Welcome <?php echo $login_session; ?> </h2> </center>
 		<br>
-		<center> <h3> <a href = "logout.php"> LOGOUT </a> </h3> </center>
+		<center>
+			<h3> <a href = "new_game.php"> NEW GAME </a> </h3>
+			<h3> <a href = "game_list.php?type=saved"> CONTINUE SAVED GAME </a> </h3>
+			<h3> <a href = "sequence_list.php?type=saved"> CONTINUE SAVED SEQUENCE </a> </h3>
+			<h3> <a href = "game_list.php?type=finished"> RECENTLY FINISHED GAMES </a> </h3>
+			<h3> <a href = "sequence_list.php?type=finished"> RECENTLY FINISHED SEQUENCES </a> </h3>
+		</center>
 	</body>
 </html>
---
+++ new_game.php
@@ -0,0 +1,51 @@
+<?php
+	
+?>
+
+<html>
+	<body>
+		<center> <h2> Game Selection </h2> </center>
+		<br>
+		<center>
+		<form action = "game.php" method = "post">
+			<table>
+				<tr> 
+				<td> LEVEL: </td>
+				<td>
+				<select name = "level">
+					<option value = "Easy"> Easy </option>
+					<option value = "Hard"> Hard </option>
+				</select>
+				</td>
+				</tr>
+				<tr> <td> </td> </tr>
+				<tr> <td> </td> </tr>
+				<tr>
+				<td> MODE: </td>
+				<td> 
+				<select name = "mode">
+					<option value = "Freestyle"> Freestyle </option>
+					<option value = "Counter"> Counter </option>
+					<option value = "Timer"> Timer </option>
+					<option value = "Sequence"> Sequence </option>
+				</select>
+				</td>
+				</tr>
+				<tr> <td> </td> </tr>
+				<tr> <td> </td> </tr>
+				<tr>
+				<td> LETTER COUNT: </td>
+				<td>
+				<select name = "letter">
+					<option value = "Four"> Four </option>
+					<option value = "Five"> Five </option>
+				</select>
+				</td>
+				</tr>
+			</table>
+			<br>
+			<input type = "submit" value = "PLAY">
+		</form>
+		</center>
+	</body>
+</html>
