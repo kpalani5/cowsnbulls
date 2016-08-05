fc1e626b24533a6a8b10358b0dad860f43f2c129
kpalani5 <kpalani5@ncsu.edu>
2016-08-04 23:00:59 -0400

    Css changes - new and pre game

--- new_game.php
+++ new_game.php
@@ -1,19 +1,67 @@
 <?php
-	
+
 ?>
 
 <html>
-	<head>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+body
+{
+	background: url('backg.jpg');
+}
+.jumbotron
+{
+	background-color:black;
+	text-align:center;
+}
+.headtext
+{
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+	color: gold;
+}
+.table
+{
+	border-color: red;
+}
+.table_txt
+{
+	color: red;
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+}
+option, select
+{
+	color: white;
+	background-color: black;
+	font-style: italic;
+	font-weight: bold;
+	font-family: "Verdana", cursive, sans-serif;
+}
+</style>
 		<title>Cows N Bulls</title>
 	</head>
-	<body bgcolor = "#00FF00">
-		<center> <h2> Game Selection </h2> </center>
+	<body>
+		<div class = "jumbotron">
+		<center> <h2 class = "headtext"> Game Selection </h2> </center>
+	</div>
+	<br>
 		<br>
 		<center>
+			<div class = "row">
+				<div class = "col-sm-4"></div>
+				<div class = "col-sm-4">
 		<form action = "pre_game.php" method = "post">
-			<table>
-				<tr> 
-				<td> LEVEL: </td>
+			<table class = "table table-bordered">
+				<tr>
+				<td class = "table_txt"> LEVEL: </td>
 				<td>
 				<select name = "level">
 					<option value = "Easy"> Easy </option>
@@ -21,11 +69,9 @@
 				</select>
 				</td>
 				</tr>
-				<tr> <td> </td> </tr>
-				<tr> <td> </td> </tr>
 				<tr>
-				<td> MODE: </td>
-				<td> 
+				<td class = "table_txt"> MODE: </td>
+				<td>
 				<select name = "mode">
 					<option value = "Freestyle"> Freestyle </option>
 					<option value = "Counter"> Counter </option>
@@ -34,10 +80,8 @@
 				</select>
 				</td>
 				</tr>
-				<tr> <td> </td> </tr>
-				<tr> <td> </td> </tr>
 				<tr>
-				<td> LETTER COUNT: </td>
+				<td class = "table_txt"> LETTER COUNT: </td>
 				<td>
 				<select name = "letter">
 					<option value = "Four"> Four </option>
@@ -46,12 +90,18 @@
 				</td>
 				</tr>
 			</table>
+		</div>
+		<div class = "col-sm-4"></div>
+	</div>
+			<br>
 			<br>
 			<input type = "hidden" value = "" name = "seq_id">
-			<input type = "submit" value = "PLAY">
+			<input type = "submit" value = "PLAY" class = "btn btn-success btn-lg">
 		</form>
+		<br>
+		<br>
 		<form action = "home.php" method = "post">
-			<input type = "submit" value = "BACK TO HOMEPAGE" id = "backbutton">
+			<input type = "submit" class = "btn btn-success btn-lg" value = "BACK TO HOMEPAGE" id = "backbutton">
 		</form>
 		</center>
 	</body>
--- pre_game.php
+++ pre_game.php
@@ -14,15 +14,15 @@
 		$sql = "SELECT count(*) FROM Wordlist WHERE Letters = '$letter' AND Difficulty = '$level';";
 		$result = mysqli_query($db,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		
+
 		$random_limit = $row["count(*)"];
 		$random_number = rand(1,$random_limit);
-		
+
 		$sql = "SELECT Word FROM Wordlist WHERE Letters = '$letter' AND Difficulty = '$level' AND TypeCount = '$random_number';";
 		$result = mysqli_query($db,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		$game_word = $row["Word"];
-		
+
 		$sql = "INSERT INTO Game(Username,Word,Status,GameTime,Mode) VALUES ('$login_user','$game_word','New',now(),'$mode');";
 		mysqli_query($db,$sql);
 
@@ -30,7 +30,7 @@
 		$result = mysqli_query($db,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		$game_id = $row["GameID"];
-		
+
 		$start_val = "START GAME";
 		$cancel_val = "CANCEL GAME";
 		$prev_game = "";
@@ -41,15 +41,15 @@
 			{
 				$sql = "INSERT INTO Sequence(GameID,GameCount,GameStatus) VALUES ('$game_id','1','New');";
 				mysqli_query($db,$sql);
-				
+
 				$sql = "SELECT SequenceID FROM Sequence WHERE GameID = '$game_id';";
 				$result = mysqli_query($db,$sql);
 				$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 				$seq_id = $row["SequenceID"];
-				
+
 				$sql = "INSERT INTO SequenceList VALUES ('$seq_id','0','$login_user','New','$letter','$level');";
 				mysqli_query($db,$sql);
-				
+
 				$start_val = "START SEQUENCE";
 			}
 			else
@@ -58,15 +58,15 @@
 				$result = mysqli_query($db,$sql);
 				$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 				$seq_count = $row["count(*)"];
-				
+
 				$sql = "SELECT GameID FROM Sequence WHERE SequenceID = '$seq_id' AND GameStatus = 'Complete';";
 				$result = mysqli_query($db,$sql);
 				$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 				$prev_game = $row["GameID"];
-				
+
 				$sql = "UPDATE Sequence SET GameStatus = 'Intermediate' WHERE GameID = '$prev_game';";
 				mysqli_query($db,$sql);
-				
+
 				$seq_count++;
 				$sql = "INSERT INTO Sequence(SequenceID,GameID,GameCount,GameStatus) VALUES ('$seq_id','$game_id','$seq_count','New');";
 				mysqli_query($db,$sql);
@@ -74,7 +74,7 @@
 				$cancel_val = "SAVE AND CONTINUE LATER";
 			}
 		}
-	}	
+	}
 	else
 	{
 		header("Location: home.php");
@@ -82,23 +82,60 @@
 ?>
 
 <html>
-	<head>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+body
+{
+	background: url('backg.jpg');
+}
+.jumbotron
+{
+	background-color:black;
+	text-align:center;
+}
+.headtext
+{
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+	color: gold;
+}
+.table
+{
+	border-color: red;
+}
+.table_txt
+{
+	color: red;
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+}
+</style>
 		<title>Cows N Bulls</title>
 	</head>
-	<body bgcolor = "#00FF00">
-		<center> <h2> Get Ready to Start the Game </h2> </center>
+	<body>
+		<div class = "jumbotron">
+		<center> <h2 class = "headtext"> Get Ready to Start the Game </h2> </center>
+	</div>
+		<br>
 		<br>
 		<form action = "game.php" method = "post">
 			<input type = "hidden" name = "mode" value = "<?php echo $mode ?>" >
 			<input type = "hidden" name = "game_id" value = "<?php echo $game_id ?>" >
 			<input type = "hidden" name = "time_left" value = "<?php echo (time()+24);?>" >
-			<center> <input type = "submit" value = "<?php echo $start_val; ?>" > </center>
+			<center> <input type = "submit" class = "btn btn-success btn-lg" value = "<?php echo $start_val; ?>" > </center>
 		</form>
 		<br>
 		<form action = "cancel_game.php" method = "post">
 			<input type = "hidden" name = "game_id" value = "<?php echo $game_id ?>" >
 			<input type = "hidden" name = "prev_game" value = "<?php echo $prev_game ?>" >
-			<center> <input type = "submit" value = "<?php echo $cancel_val; ?>" > </center>
+			<center> <input type = "submit" class = "btn btn-success btn-lg" value = "<?php echo $cancel_val; ?>" > </center>
 		</form>
 	</body>
 </html>
