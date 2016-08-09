5958ff58c9fb1d959b06a3fa6feecf3078f7a06e
kpalani5 <kpalani5@ncsu.edu>
2016-08-08 21:57:15 -0400

    CSS Stage 1

--- game_list.php
+++ game_list.php
@@ -10,23 +10,62 @@
 	}
 	else
 	{
-		$sql = "SELECT GameID,GameTime,Status FROM Game WHERE Username = '$login_user' AND Status IN ('Success','Failure');";
+		$sql = "SELECT GameID,GameTime,Status FROM Game WHERE Username = '$login_user' AND Status IN ('Success','Failure') ORDER BY GameTime DESC LIMIT 10;";
 		$val = "VIEW GAME";
 	}
 	$result = mysqli_query($db,$sql);
-		
+
 ?>
 
 <html>
-	<head>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+body
+{
+	background: url('backg.jpg');
+}
+.jumbotron
+{
+	background-color:black;
+	text-align:center;
+}
+.headtext
+{
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+	color: gold;
+}
+.table
+{
+	border-color: red;
+}
+.table_txt
+{
+	color: red;
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+}
+</style>
 		<title>Cows N Bulls</title>
 	</head>
-	<body bgcolor = "#00FF00">
+	<body>
 		<center>
-		<h1> LIST OF GAMES </h1>
+		<div class = "jumbotron">
+		<h2 class = "headtext"> LIST OF GAMES </h2>
+	</div>
 		<br>
 		<br>
-		<table border = "2">
+		<div class = "row">
+			<div class = "col-sm-4"> </div>
+			<div class = "col-sm-4">
+		<table class = "table table-bordered">
 			<th> Game ID </th>
 			<th> Game Start Time </th>
 			<th> Game Status </th>
@@ -36,21 +75,24 @@
 					{
 						echo "<tr>";
 						echo "<form action = 'game.php' method = 'post'>";
-						echo "<td>" . $row["GameID"] . "</td>";
-						echo "<td>" . $row["GameTime"] . "</td>";
-						echo "<td>" . $row["Status"] . "</td>";
+						echo "<td class = 'table_txt'>" . $row["GameID"] . "</td>";
+						echo "<td class = 'table_txt'>" . $row["GameTime"] . "</td>";
+						echo "<td class = 'table_txt'>" . $row["Status"] . "</td>";
 						echo "<input type = 'hidden' id = 'game_id' name = 'game_id' value = " . $row["GameID"] . ">";
 						echo "<input type = 'hidden' name = 'time_left' id = 'time_left' value = ''>";
-						echo "<td> <input type = 'submit' value = " . $val . "> </td>";
+						echo "<td> <input type = 'submit' class = 'btn btn-lg btn-success' value = " . $val . "> </td>";
 						echo "</form>";
 						echo "</tr>";
 					}
 				?>
 		</table>
+	</div>
+	<div class = "col-sm-4"> </div>
+</div>
 		<br>
 		<br>
 		<form action = "home.php" method = "post">
-			<input type = "submit" value = "BACK TO HOMEPAGE" id = "backbutton">
+			<input type = "submit" class = "btn btn-lg btn-success" value = "BACK TO HOMEPAGE" id = "backbutton">
 		</form>
 		</center>
 	</body>
--- sequence_list.php
+++ sequence_list.php
@@ -5,25 +5,64 @@
 	$type = $_GET['type'];
 	if($type == "saved")
 	{
-		$sql = "SELECT SequenceID,Score,Letters,Difficulty FROM SequenceList WHERE Username = '$login_user' AND SequenceStatus = 'Open';";	
+		$sql = "SELECT SequenceID,Score,Letters,Difficulty FROM SequenceList WHERE Username = '$login_user' AND SequenceStatus = 'Open';";
 	}
 	else
 	{
-		$sql = "SELECT SequenceID,Score,Letters,Difficulty FROM SequenceList WHERE Username = '$login_user' AND SequenceStatus = 'Finished';";	
+		$sql = "SELECT SequenceID,Score,Letters,Difficulty FROM SequenceList WHERE Username = '$login_user' AND SequenceStatus = 'Finished';";
 	}
 	$result = mysqli_query($db,$sql);
 ?>
 
 <html>
-	<head>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+body
+{
+	background: url('backg.jpg');
+}
+.jumbotron
+{
+	background-color:black;
+	text-align:center;
+}
+.headtext
+{
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+	color: gold;
+}
+.table
+{
+	border-color: red;
+}
+.table_txt
+{
+	color: red;
+	font-style: oblique;
+	font-weight: bold;
+	font-family: "Comic Sans MS", cursive, sans-serif;
+}
+</style>
 		<title>Cows N Bulls</title>
 	</head>
-	<body bgcolor = "#00FF00">
+	<body>
 		<center>
-		<h1> LIST OF SEQUENCES </h1>
+      <div class = "jumbotron">
+		<h2 class = "headtext"> LIST OF SEQUENCES </h2>
+  </div>
 		<br>
 		<br>
-		<table border = "2">
+    <div class = "row">
+      <div class = "col-sm-4"> </div>
+      <div class = "col-sm-4">
+		<table class = "table table-bordered">
 			<th> Sequence ID </th>
 			<th> Score </th>
 			<th>  </th>
@@ -37,26 +76,29 @@
 						}
 						echo "<tr>";
 						echo "<form action = 'pre_game.php' method = 'post'>";
-						echo "<td>" . $row["SequenceID"] . "</td>";
-						echo "<td>" . $row["Score"] . "</td>";
+						echo "<td class = 'table_txt'>" . $row["SequenceID"] . "</td>";
+						echo "<td class = 'table_txt'>" . $row["Score"] . "</td>";
 						echo "<input type = 'hidden' id = 'seq_id' name = 'seq_id' value = " . $row["SequenceID"] . ">";
 						echo "<input type = 'hidden' id = 'level' name = 'level' value = " . $row["Difficulty"] . ">";
 						echo "<input type = 'hidden' id = 'letter' name = 'letter' value = " . $letter . ">";
 						echo "<input type = 'hidden' id = 'mode' name = 'mode' value = 'Sequence'>";
 						if($type == "saved")
 						{
-							echo "<td> <input type = 'submit' value = 'CONTINUE SEQUENCE'> </td>";
+							echo "<td> <input type = 'submit' class = 'btn btn-lg btn-success' value = 'CONTINUE SEQUENCE'> </td>";
 						}
 						echo "</form>";
 						echo "</tr>";
 					}
 				?>
 		</table>
+  </div>
+  <div class = "col-sm-4"></div>
+</div>
 		<br>
 		<br>
 		<form action = "home.php" method = "post">
-			<input type = "submit" value = "BACK TO HOMEPAGE" id = "backbutton">
+			<input type = "submit" class = "btn btn-lg btn-success" value = "BACK TO HOMEPAGE" id = "backbutton">
 		</form>
-		</center>		
+		</center>
 	</body>
 </html>
